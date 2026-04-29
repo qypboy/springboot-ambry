85e414b9852b3f54460591de4ddd2bc013105b7c
qiuzhanwen <zhanwen.qiu@transsion.com>
2026-04-29 15:00:08 +0800

    feat: 优化

--- src/main/java/com/example/mapper/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.Product;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ProductMapper extends BaseMapper<Product> {
-}
--- src/main/java/com/example/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.entity.Product;
-
-public interface ProductService extends IService<Product> {
-}
